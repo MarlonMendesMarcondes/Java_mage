6a9204a7da1d1f4718506ecd28a0e478c49f6cde
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-03-30 23:14:31 -0300

    Blocos de Inicializãção

--- untitled/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,18 +5,28 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime(String nome, String tipo, int episodios, String genero){
-        System.out.println("Dentro do Construtor");
+    //    System.out.println("Dentro do Construtor");
+
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
+        this.genero = genero;
+    }
+    public Anime(String nome, String tipo, int episodios, String genero,String estudio){
+        this(nome,tipo,episodios,genero);
+        this.estudio =  estudio;
     }
     public void imprime(){
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
+        System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
+
 
     public void setNome(String nome) {
         this.nome = nome;
--- untitled/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Boruto","TV",12,"ação");
+        Anime anime = new Anime("Boruto","TV",12,"ação", "Production IG");
         anime.imprime();
     }
 
