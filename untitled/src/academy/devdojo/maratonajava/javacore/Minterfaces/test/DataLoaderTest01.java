5217982a9ee87576ab7868e7bffba86787b03f65
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-05-17 17:30:19 -0300

    Atualizacao data 17/05/2023

---
+++ untitled/src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.test;
+
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.Dataloader;
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
+
+
+public class DataLoaderTest01 {
+    public static void main(String[] args) {
+        DatabaseLoader databaseLoader = new DatabaseLoader();
+        FileLoader fileLoader = new FileLoader();
+        databaseLoader.load();
+        fileLoader.load();
+
+        databaseLoader.remove();
+        fileLoader.remove();
+
+        databaseLoader.checkPermission();
+        fileLoader.checkPermission();
+
+        Dataloader.retrieveMaxDataSize();
+        DatabaseLoader.retrieveMaxDataSize();
+
+    }
+}
